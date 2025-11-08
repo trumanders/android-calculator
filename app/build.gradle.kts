df2d08b2156aaf8b0a89b079b168caf570863e4b
Anders Sven Axel Johansson <anderssvenaxelj@gmail.com>
2025-11-08 21:42:21 +0100

    Update versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jazasoftware.instantcalculator"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
